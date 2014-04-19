5b2ec82fcd0481b1e407499c8a58d39a009617db
Nicholas Smith <nicholasparkersmith@gmail.com>
2014-04-19 18:37:20 -0400

    better functionality and fixing opening the widget after saving

--- script.js
+++ script.js
@@ -2,47 +2,30 @@
 * Description:
 * Media uploader via WP 3.5
 *
-* @method suwUploader
+* @method void
 * @param  void
 * @return false
 */
+(function($) {
+    $(function() {
 
-function suwUploader() {
-    var _custom_media = true,
-    _orig_send_attachment = wp.media.editor.send.attachment;
+        var _custom_media = true,
+            _orig_send_attachment = wp.media.editor.send.attachment;
 
-    jQuery('input.suw-button-select').click(function(e) {
-        var send_attachment_bkp, button, input, value;
-        send_attachment_bkp = wp.media.editor.send.attachment;
-        button = jQuery(this);
-        input = jQuery(this).prev('.suw-input-field');
-        _custom_media = true;
-        wp.media.editor.send.attachment = function(props, attachment){
-            if ( _custom_media ) {
-                input.val(attachment.url);
-            } else {
-                return _orig_send_attachment.apply( this, [props, attachment] );
-            };
-        }
-        wp.media.editor.open(button);
-        return false;
+        $(document).on('click', '.suw-button-select.button', function () {
+            var send_attachment_bkp = wp.media.editor.send.attachment,
+                input = $(this).prev('.suw-input-field');
+            _custom_media = true;
+            wp.media.editor.send.attachment = function(props, attachment){
+                if ( _custom_media ) {
+                    input.val(attachment.url);
+                } else {
+                    return _orig_send_attachment.apply( this, [props, attachment] );
+                };
+            }
+            wp.media.editor.open( $(this) );
+            return false;
+        });
+
     });
-
-    jQuery('.add_media').on('click', function(){
-        _custom_media = false;
-    });
-};
-
-
-/*
-* Description:
-* jQuery call on above function
-*
-* @method void
-* @param  void
-* @return void
-*/
-
-jQuery(document).ready(function($){
-    suwUploader();
-});
+})(jQuery);
